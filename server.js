2b1ebb0c88f7ceba867ffce555bdcea0c1fc972b
Martin Ortega <martin.ortega@mail.rakuten.com>
2013-05-03 15:43:28 -0700

    Changes the server to use express.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-var http = require('http')
-  , port = process.env.PORT || 1337;
+var express = require('express')
+  , port = process.env.PORT || 8889;
 
-http.createServer(function (req, res) {
+var server = express();
 
+server.get('/', function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('Hello, World!');
+  res.write('Hello, World! I am now using express.js!');
   res.end();
+});
 
-}).listen(port);
+server.listen(port);
