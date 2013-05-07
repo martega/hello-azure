7af7c272bcdca48e3cf7524fe80a9827c0cdfd68
Martin Ortega <martin.ortega@mail.rakuten.com>
2013-05-06 18:24:57 -0700

    Renames server.js to main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var server = express();
 
 server.get('/', function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('Hello, World! I am now using express.js!');
+  res.write('Hello, World! I am now using express.js! Wow, it worked!');
   res.end();
 });
 
